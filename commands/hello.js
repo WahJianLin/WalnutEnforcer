27f01cdf072692c433662d3e511a181edc9e3d90
mrtaoism <taoz96@gmail.com>
2020-02-12 15:11:12 -0500

    more progress with timeout and removed token online

--- commands/hello.js
+++ commands/hello.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-      return message.channel.send("Hola, mi amigo");  
+      return message.channel.send("Hola, mi amigo");
 }
 
 module.exports.help = {
--- commands/timeout.js
+++ commands/timeout.js
@@ -2,10 +2,13 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
   //Check if the user has the correct role
-  if(!message.author.hasRole("Saul Only")){
-    message.channel.send("You do can't do that.")
+  let saulrole = message.guild.roles.find(role => role.name === "Saul Only");
+  if(!message.member.roles.has(saulrole)){
+    message.channel.send("You can't do that.")
     return;
   }
+
+
 }
 
 module.exports.help = {
