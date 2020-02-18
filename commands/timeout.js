3f522f175fe6bf2a4eb24c0b739d291c317ea561
mrtaoism <taoz96@gmail.com>
2020-02-18 11:47:08 -0500

    added initial timeout functionality

--- commands/timeout.js
+++ commands/timeout.js
@@ -1,12 +1,33 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-  //Check if the user has the correct role
-  let saulrole = message.guild.roles.find(role => role.name === "Saul Only");
-  if(!message.member.roles.has(saulrole)){
-    message.channel.send("You can't do that.")
-    return;
+  // Check if the user has the correct role
+  let saulrole = message.guild.roles.find(role => role.name === "Saul");
+  if(!message.member.roles.has(saulrole.id)){
+    return message.channel.send("You can't do that.");
   }
+
+  // Check if user is tagged
+  let rMember = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+  if(!rMember) return message.channel.send("Who you tryna jail?");
+
+  // Jail em
+  const afkChan = message.guild.afkChannel;
+
+  // message.channel.send(afkChan.name);
+
+  await rMember.setVoiceChannel(afkChan);
+  return message.react('👌');
+
+  // try{
+  //   await member.setVoiceChannel(afkChan);
+  //   message.react('👌');
+  // }
+  // catch(e){
+  //   return;
+  // }
+
+
 
 
 }
