838bfcd9d073150a7ec92151c537571fcf307546
Jian Lin <wahjian.lin@gmail.com>
2021-04-03 16:55:36 -0400

    Updated for discord.js 12

---
+++ commands/jail.js
@@ -0,0 +1,80 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+  //get role ids and checks if typer has roles
+  let targetRole = message.guild.roles.cache.find(
+    (role) => role.name === "Enforcer"
+  );
+  let corruptRole = message.guild.roles.cache.find(
+    (role) => role.name === "Corrupt Enforcer"
+  );
+  let hasTargetRole = message.member.roles.cache.has(targetRole.id);
+  let hasCorruptRole = message.member.roles.cache.has(corruptRole.id);
+
+  //finds target user and if they are corrupt
+  let targetUser = message.guild.member(message.mentions.users.first());
+  let corruptTargetRole = targetUser.roles.cache.find(
+    (role) => role.name === "Corrupt Enforcer"
+  );
+
+  //checks if target user is corrupt
+  if (corruptTargetRole == corruptRole.id) {
+    return message.channel.send("No");
+  }
+
+  //checks if user is an enforcer
+  if (!hasTargetRole && !hasCorruptRole) {
+    return message.channel.send("You are not an Enforcer");
+  }
+
+  //checks if target is in voice channel
+  if (!targetUser.voice.channel) {
+    return message.channel.send("Convict does not exist or escaped");
+  }
+
+  //gets channels and timers
+  const afkChan = message.guild.afkChannel;
+  let ogChan = targetUser.voice.channel;
+  var timer;
+
+  startJail();
+
+  //puts user in afk, starts the repeater to put users in afk, and setstimer to stop the repeater
+  function startJail() {
+    targetUser.voice.setChannel(afkChan);
+    message.channel.send("Go to jail you criminal fiend");
+    jail();
+    setTimeout(stopJail, 24 * 1000);
+  }
+
+  //starts repeater to put user in afk if they are not in afk channel
+  function jail() {
+    timer = setInterval(function () {
+      if (targetUser.voice.channel != afkChan) {
+        targetUser.voice.setChannel(afkChan);
+        if (!targetUser.voice.channel) {
+          message.channel.send("The Criminal is on the run");
+        } else {
+          message.channel.send("Get back to jail you SCUM of the Earth");
+        }
+      }
+    }, 2 * 1000);
+  }
+
+  //stops the repeater and moves user back to their original channel
+  function stopJail() {
+    clearInterval(timer);
+    targetUser.voice.setChannel(ogChan);
+    if (!targetUser.voice.channel) {
+      message.channel.send("The dirty outlaw has escaped");
+    } else {
+      message.channel.send(
+        "Your time has been served. You are free, but you better behave yourself"
+      );
+    }
+  }
+};
+
+module.exports.help = {
+  name: "jail",
+};
---
+++ commands/timeoutOld.js
@@ -0,0 +1,50 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+  return;
+  /*
+  // Check if the user has the correct role
+  let saulrole = message.guild.roles.find((role) => role.name === "Saul");
+  if (!message.member.roles.has(saulrole.id)) {
+    return message.channel.send("You can't do that.");
+  }
+
+  // Check if user is tagged
+  let rMember = message.guild.member(
+    message.mentions.users.first() || message.guild.members.get(args[0])
+  );
+  if (!rMember) return message.channel.send("Who you tryna jail?");
+
+  // Jail em
+  const afkChan = message.guild.afkChannel;
+
+  function jail() {
+    if (rMember.voiceChannel.id != afkChan) {
+      rMember.setVoiceChannel(afkChan);
+      // message.channel.send('👌');
+    }
+  }
+
+  function stopJail() {
+    clearInterval(myTimer);
+    return message.channel.send("They've done their time.");
+  }
+
+  var myTimer = setInterval(jail, 10 * 1000);
+  setTimeout(stopJail, 1 * 60 * 1000);
+  */
+  // message.channel.send(afkChan.name);
+  // setTimeout(setInterval(jail(), 10*1000), 1*60*1000);
+
+  // try{
+  //   await member.setVoiceChannel(afkChan);
+  //   message.react('👌');
+  // }
+  // catch(e){
+  //   return;
+  // }
+};
+
+module.exports.help = {
+  name: "timeoutOld",
+};
