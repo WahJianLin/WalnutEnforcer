f0067500b0b1e88806e7812f85875441c7ee5db8
Jian Lin <wahjian.lin@gmail.com>
2021-04-04 21:40:04 -0400

    Connecting to Heroku

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
-const bot = new Discord.Client({disableEveryone: true});
+const bot = new Discord.Client({ disableEveryone: true });
 const fs = require("fs");
 bot.commands = new Discord.Collection();
 
 fs.readdir("./commands/", (err, files) => {
-  if(err) console.log(err);
+  if (err) console.log(err);
 
-  let jsfile = files.filter(f => f.split(".").pop() === "js")
-  if(jsfile.length <= 0){
+  let jsfile = files.filter((f) => f.split(".").pop() === "js");
+  if (jsfile.length <= 0) {
     console.log("Couldn't find commands.");
     return;
   }
 
-  jsfile.forEach((f, i) =>{
+  jsfile.forEach((f, i) => {
     let props = require(`./commands/${f}`);
     console.log(`${f} loaded!`);
     bot.commands.set(props.help.name, props);
@@ -24,9 +24,9 @@ bot.on("ready", async () => {
   console.log(`${bot.user.username} is online!`);
 });
 
-bot.on("message", async message => {
-  if(message.author.bot) return;
-  if(message.channel.type == "dm") return;
+bot.on("message", async (message) => {
+  if (message.author.bot) return;
+  if (message.channel.type == "dm") return;
 
   let prefix = "!";
   let messageArray = message.content.split(" ");
@@ -34,11 +34,11 @@ bot.on("message", async message => {
   let args = messageArray[1];
 
   let commandfile = bot.commands.get(cmd.slice(prefix.length));
-  if(commandfile) commandfile.run(bot, message, args);
+  if (commandfile) commandfile.run(bot, message, args);
 
   // if(cmd == `${prefix}hello`){
   //     return message.channel.send("Hi");
   // }
 });
 
-bot.login(botconfig.token);
+bot.login(process.env.WALNUT_TOKEN);
