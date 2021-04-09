da40eeb9f07521da17ed8dc79aeb3a9f2d8d07ae
Jian Lin <wahjian.lin@gmail.com>
2021-04-08 21:43:41 -0400

    Added Audio Capabilities v1

---
+++ commands/audio.js
@@ -0,0 +1,45 @@
+const Discord = require("discord.js");
+const path = require("path");
+
+module.exports.run = async (bot, message, args) => {
+  //list of audio files
+  let audioArr = ["pikachu", "test"];
+
+  //gets caller's channel and checks if user is in channel
+  let callerChan = message.member.voice.channel;
+  if (!callerChan) {
+    return message.channel.send("User is not in a voice channel");
+  }
+
+  //checks if user hasa valid audio request
+  let msg = message.toString();
+  msg = msg.split(" ");
+  if (audioArr.indexOf(msg[1]) == -1) {
+    help();
+    return;
+  }
+
+  //makes bot join caller's channel and play audio. Disconnects after 10 seconds
+  const connection = await callerChan.join();
+  const dispatcher = connection.play(
+    path.join(__dirname, "..", "audio", msg[1].toLowerCase() + ".mp3")
+  );
+  setTimeout(chanDisconnect, 3 * 1000);
+
+  return;
+  function chanDisconnect() {
+    callerChan.leave();
+  }
+  function help() {
+    let str =
+      "To play audio please type in '!audio {target clip}'\n\nTarget Clips:";
+    for (let i = 0; i < audioArr.length; i++) {
+      str = str + "\n    '" + audioArr[i] + "'";
+    }
+    message.channel.send("" + str);
+  }
+};
+
+module.exports.help = {
+  name: "audio",
+};
